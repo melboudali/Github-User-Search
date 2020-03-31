d0afd520e62b0123cb3b741e86b767c1ebc75349
Med Elboudali <med.sneaky@gmail.com>
2020-03-31 13:28:06 +0000

    Edit and styling About page

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -6,19 +6,32 @@ const About = () => {
   return (
     <Fragment>
       <Container>
-        <Col xs="12" sm="12" md="12" lg="8" xl="8">
+        <Col xs="12" sm="12" md="12" lg="12" xl="12">
           <h2 className="AboutHeader">About Page</h2>
-          <h6>
+          <h6 className="subHeader">
             Github user search using react, react-dom, react-bootstrap,
             react-router-bootstrap, axios and Github Api.
           </h6>
-          <h7 className="githubDescription">
-          <i className="fab fa-github"/> GitHub, Inc. is a US-based global company that provides hosting for
-            software development version control using Git. It is a subsidiary
-            of Microsoft, which acquired the company in 2018 for US$7.5 billion.
-            It offers the distributed version control and source code management
-            functionality of Git, plus its own features.
-          </h7>
+          <div className="githubDescriptionCont">
+            <h7 className="githubDescription">
+              <i className="fab fa-github" /> GitHub, Inc. is a US-based global
+              company that provides hosting for software development version
+              control using Git. It is a subsidiary of Microsoft, which acquired
+              the company in 2018 for US$7.5 billion. It offers the distributed
+              version control and source code management functionality of Git,
+              plus its own features.
+            </h7>
+          </div>
+          <h6 className="repo">
+            Github Repo:
+            <a
+              className="githubLink"
+              href="https://github.com/MedElBoudali/Github-User-Search"
+              target="a_blank"
+            >
+              Click Here
+            </a>
+          </h6>
         </Col>
       </Container>
     </Fragment>
