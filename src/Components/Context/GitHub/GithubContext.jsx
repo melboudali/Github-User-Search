914fd14287bd6cb6dfdcea6da675b0b225126bca
MedElBoudali <med.sneaky@gmail.com>
2021-01-05 16:29:00 +0000

    add context displayName

--- src/Components/Context/GitHub/GithubContext.jsx
+++ src/Components/Context/GitHub/GithubContext.jsx
@@ -1,5 +1,7 @@
-import {createContext} from "react";
+import { createContext } from 'react';
 
 const githubContext = createContext();
+
+githubContext.displayName = 'Github Context';
 
 export default githubContext;
